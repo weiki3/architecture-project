ed1d069cf73f8da908fa23c4bd566d549348cda7
weiki <wikii3@outlook.com>
2024-01-14 20:53:32 +0800

    modifying

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,55 +1,49 @@
 #include "../include/utils.h"
 
-class Checker
+Checker::Checker(vector<int> exp_ans) : expected_answer(exp_ans) {}
+
+pair<int, int> Checker::count_visited_vertices(const vector<int> &distance)
 {
-    vector<int> expected_answer;
-
-public:
-    Checker(vector<int> exp_ans) : expected_answer(exp_ans) {}
-
-    pair<int, int> count_visited_vertices(const vector<int> &distance)
+    int depth = 0;
+    int count = 0;
+    for (int x : distance)
     {
-        int depth = 0;
-        int count = 0;
-        for (int x : distance)
+        if (x < INT_MAX)
         {
-            if (x < INT_MAX)
+            ++count;
+            if (x > depth)
             {
-                ++count;
-                if (x > depth)
-                {
-                    depth = x;
-                }
+                depth = x;
             }
         }
-        return {count, depth};
     }
+    return {count, depth};
+}
 
-    void check(vector<int> answer)
+void Checker::check(vector<int> answer)
+{
+    assert(answer.size() == expected_answer.size());
+    bool is_ok = true;
+    int position_wrong = -1;
+    for (int i = 0; i < answer.size(); ++i)
     {
-        assert(answer.size() == expected_answer.size());
-        bool is_ok = true;
-        int position_wrong = -1;
-        for (int i = 0; i < answer.size(); ++i)
+        if (answer.at(i) != expected_answer.at(i))
         {
-            if (answer.at(i) != expected_answer.at(i))
-            {
-                is_ok = false;
-                position_wrong = i;
-                break;
-            }
-        }
-        if (is_ok)
-        {
-            pair<int, int> graph_output = count_visited_vertices(answer);
-            int n_visited_vertices = graph_output.first;
-            int depth = graph_output.second;
-            printf("CHECKED SUCCESSFULY! Number of is_visited vertices: %i, depth: %i \n", n_visited_vertices, depth);
-        }
-        else
-        {
-            printf("Something went wrong!\n");
-            printf("Answer at %i equals %i but should be equal to %i\n", position_wrong, answer[position_wrong], expected_answer[position_wrong]);
+            is_ok = false;
+            position_wrong = i;
+            break;
         }
     }
-};
+    if (is_ok)
+    {
+        pair<int, int> graph_output = count_visited_vertices(answer);
+        int n_visited_vertices = graph_output.first;
+        int depth = graph_output.second;
+        printf("CHECKED SUCCESSFULY! Number of is_visited vertices: %i, depth: %i \n", n_visited_vertices, depth);
+    }
+    else
+    {
+        printf("Something went wrong!\n");
+        printf("Answer at %i equals %i but should be equal to %i\n", position_wrong, answer[position_wrong], expected_answer[position_wrong]);
+    }
+}

0d063ebb7c84c1188def0644183be19010e776a3
weiki <wikii3@outlook.com>
2024-01-14 21:27:57 +0800

    modifying

--- make.sh
+++ make.sh
@@ -1,2 +1,14 @@
+#!/bin/bash
+
+# 创建 bin 目录（如果不存在）
+if [ ! -d "bin" ]; then
+    mkdir bin
+fi
+
 nvcc -std=c++11 src/main.cpp src/graph.cpp src/cuda_bfs.cu src/cpu_bfs.cpp src/utils.cpp -o ./bin/bfs_test
-&& echo "成功完成 cuda 编译"
+
+if [ $? -eq 0 ]; then
+    echo "编译成功，可执行文件 bin/bfs_test"
+else
+    echo "编译失败"
+fi
